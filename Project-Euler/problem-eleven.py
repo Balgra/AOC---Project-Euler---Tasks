d46df3aacbf48b1ce02d73063a40848f22fd504f
Balgra <andreibalgradean16@gmail.com>
2026-01-22 22:39:00 +0100

    Problem 12 & Test Problem 11

--- Project-Euler/problem-eleven.py
+++ Project-Euler/problem-eleven.py
@@ -1,3 +1,14 @@
+
+# In the  20x20  grid below, four numbers along a diagonal line have been marked in red.
+
+# What is the greatest product of four adjacent numbers in the same direction (up, down, left, right, or diagonally) in the 20x20  grid?
+
+
+
+
+
+
+
 matrix = [
     [8, 2, 22, 97, 38, 15, 0, 40, 0, 75, 4, 5, 7, 78, 52, 12, 50, 77, 91, 8],
     [49, 49, 99, 40, 17, 81, 18, 57, 60, 87, 17, 40, 98, 43, 69, 48, 4, 56, 62, 0],
